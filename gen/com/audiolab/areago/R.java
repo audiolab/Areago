dd891c0b70afac86dc9eaa44d2c0305d8d147236
Siroco <siroco@riseup.net>
2012-11-06 20:16:27 +0100

    Ultima actualización

--- gen/com/audiolab/areago/R.java
+++ gen/com/audiolab/areago/R.java
@@ -14,18 +14,34 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int brujula=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int audifonos=0x7f020000;
+        public static final int brujula=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int button1=0x7f080000;
-        public static final int exit=0x7f080002;
-        public static final int preferences=0x7f080001;
+        public static final int descargar=0x7f08000a;
+        public static final int descripcion=0x7f080007;
+        public static final int ejecutar=0x7f08000b;
+        public static final int exit=0x7f08000f;
+        public static final int gps=0x7f080003;
+        public static final int horizontalScrollView1=0x7f08000c;
+        public static final int idioma=0x7f080009;
+        public static final int imageView1=0x7f080000;
+        public static final int layout_general=0x7f08000d;
+        public static final int lbldescripcion=0x7f080006;
+        public static final int lblidioma=0x7f080008;
+        public static final int lbltitulo=0x7f080004;
+        public static final int preferences=0x7f08000e;
+        public static final int textView1=0x7f080001;
+        public static final int titulo=0x7f080005;
+        public static final int wireless=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int list_activity=0x7f030001;
+        public static final int paseo_preview=0x7f030002;
+        public static final int scroll_walks=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
--- src/com/audiolab/areago/MainActivity.java
+++ src/com/audiolab/areago/MainActivity.java
@@ -7,21 +7,32 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	ProgressDialog dialog;
 	String url = "http://www.xavierbalderas.com/areago/areago/listado";
+	String string="";
+	LocationManager locManager;
+	LocationListener locationListener;
+	Double lat;
+	Double lon;
 	
 	private InputStream OpenHttpConnection (String urlString) throws IOException {
 		InputStream in = null;
@@ -50,57 +61,158 @@ public class MainActivity extends Activity {
 		return in;
 	}
 	
-	
-	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.imageView1).setClickable(false);
+        
+        
         setTitle("AREAGO : Inicio");
+        //Wireless
+        dialog = ProgressDialog.show(this,"Comprobando Wifi","Espera...",true);
+        
+        if (!init_wireless()) { 
+        	((TextView)findViewById(R.id.wireless)).setText("Wireless Inactivo");
+        }
+        else { 
+        	((TextView)findViewById(R.id.wireless)).setText("Wireless Activado");
+        	findViewById(R.id.imageView1).setClickable(true);
+        	}
+        dialog.dismiss();
+        
+        //GPS
+        ((TextView)findViewById(R.id.gps)).setText("Inciando recepción de datos GPS");
+        init_gps();
+        
     }
     
-    public void onClick(View view) {
-    	
-    				dialog = ProgressDialog.show(MainActivity.this, "Cargando datos", "Espera mientras cargamos los datos de las rutas...", true);
+    private void init_gps() {
+		// TODO Auto-generated method stub
+    	locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    	locationListener = new AreagoLocationListener();
+    	Criteria crit = new Criteria();
+    	crit.setAccuracy(Criteria.ACCURACY_FINE);
+    	locManager.getLastKnownLocation(locManager.getBestProvider(crit, true));
+	}
 
-        			int BUFFER_SIZE = 2000;
-        			InputStream in = null;
-        			String str="";
-        			try {
-        				in = OpenHttpConnection(url);
-        			} catch (Exception e) {
-        				Log.d("Networking",e.getLocalizedMessage());
-        				str = "";
-        				Toast.makeText(this,"No se puede conectar con el servidor",Toast.LENGTH_LONG).show();
-        				dialog.dismiss();
-        				return;
-        			}
-        			
-        			InputStreamReader isr = new InputStreamReader(in);
-        			int charRead;
-        			
-        			char[] inputBuffer = new char[BUFFER_SIZE];
-        			try {
-        				while ((charRead = isr.read(inputBuffer))>0) {
-        					String readString = String.copyValueOf(inputBuffer,0,charRead);
-        					str += readString;
-        					inputBuffer = new char[BUFFER_SIZE];
-        				}
-        				in.close();
-        			} catch (IOException e) {
-        				Log.d("Networking",e.getLocalizedMessage());
-        				str = "";
-        				return;
-        			}   			
-        			
-        			dialog.dismiss();
-        		
-        			Intent i = new Intent("com.audiolab.areago.ListActivityPaseos");
-        			i.putExtra("json", str);
-        	
-        			Toast.makeText(this,"Datos cargados con éxito",Toast.LENGTH_LONG).show();
-        		
-   			 		startActivity(i);
+    private class AreagoLocationListener implements LocationListener {
+    	
+    	public void onLocationChanged(Location location) {
+    		// TODO Auto-generated method stub
+    		if (location != null) {
+    			//Toast.makeText(getBaseContext(),"Location changed",Toast.LENGTH_LONG).show();
+    			((TextView)findViewById(R.id.gps)).setText("Posición: "+location.getLatitude()+"/"+location.getLongitude()+"/"+location.getAccuracy());
+    			lat = location.getLatitude();
+    			lon = location.getLongitude();
+    		}
+    		
+    	}
+
+    	public void onProviderDisabled(String provider) {
+    		// TODO Auto-generated method stub
+    		((TextView)findViewById(R.id.gps)).setText(provider+" desconectado");
+    		findViewById(R.id.imageView1).setClickable(false);
+    		
+    	}
+
+    	public void onProviderEnabled(String provider) {
+    		// TODO Auto-generated method stub
+    		((TextView)findViewById(R.id.gps)).setText("GPS Conectado:"+provider);
+    		if (findViewById(R.id.imageView1).isClickable()) findViewById(R.id.imageView1).setClickable(true);
+
+    	}
+
+    	public void onStatusChanged(String provider, int status, Bundle extras) {
+    		// TODO Auto-generated method stub
+    		String st = "";
+    		switch (status) {
+    		case android.location.LocationProvider.AVAILABLE:
+    			st="Disponible";
+    		case android.location.LocationProvider.OUT_OF_SERVICE:
+    			st="Desactivado";
+    		case android.location.LocationProvider.TEMPORARILY_UNAVAILABLE:
+    			st="Temporalmente desactivado";
+    		}
+    		((TextView)findViewById(R.id.gps)).setText("GPS Status:"+st);
+    	}
+
+    }
+
+
+	private boolean init_wireless() {
+		// TODO Auto-generated method stub
+    	ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+    	NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    	return mWifi.isConnected();
+    	//return false;
+	}
+
+public void onResume() {
+
+	super.onResume();
+	locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+}
+
+public void onPause() {
+	super.onPause();
+	locManager.removeUpdates(locationListener);
+}
+
+	public void onClick(View view) {
+    	
+    				final ProgressDialog dialog = ProgressDialog.show(this,"Cargando rutas","Espera...",true);
+    				
+    				new Thread(new Runnable(){
+    					public void run(){
+    					try {
+    							//Thread.sleep(5000);
+    							string = init_rutas();
+    							Intent i = new Intent("com.audiolab.areago.ListActivityPaseos");
+    		        			i.putExtra("json", string);
+    		        			i.putExtra("lat", lat);
+    		        			i.putExtra("lon", lon);
+    							dialog.dismiss();
+    							startActivity(i);
+    						} catch (Exception e) {
+    							e.printStackTrace();
+    						}
+    					}
+
+						private String init_rutas() {
+							// TODO Auto-generated method stub
+							int BUFFER_SIZE = 2000;
+							String str = "";
+		        			InputStream in = null;
+		        			try {
+		        				in = OpenHttpConnection(url);
+		        			} catch (Exception e) {
+		        				Log.d("Networking",e.getLocalizedMessage());
+		        				dialog.dismiss();
+		        				return str;
+		        			}
+		        			
+		        			InputStreamReader isr = new InputStreamReader(in);
+		        			int charRead;
+		        			
+		        			char[] inputBuffer = new char[BUFFER_SIZE];
+		        			try {
+		        				while ((charRead = isr.read(inputBuffer))>0) {
+		        					String readString = String.copyValueOf(inputBuffer,0,charRead);
+		        					str += readString;
+		        					inputBuffer = new char[BUFFER_SIZE];
+		        				}
+		        				in.close();
+		        			} catch (IOException e) {
+		        				Log.d("Networking",e.getLocalizedMessage());
+		        				str = "";
+		        				return str;
+		        			}   			
+		        			
+		        			dialog.dismiss();
+							return str;
+						}
+    				}).start();
     }
     
     @Override
@@ -124,7 +236,4 @@ public class MainActivity extends Activity {
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
-    
-    //Carga de los datos de preferencias y las rutas
-
 }
